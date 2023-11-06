7ba26490a3c3ea674044e801c99c3536ea2602f4
Kennedy-oss <144157641+Kennedy-oss@users.noreply.github.com>
2023-11-06 17:42:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,97 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    function calculateMaterials(event) {
-        event.preventDefault();
-        
-        // Get user input from the form
-        let projectType = document.getElementById('project-type').value;
-        let dimensions = document.getElementById('dimensions').value;
-        let units = document.getElementById('units').value;
+// Replace 'your_api_key_here' with your actual API key from Apacta settings
+const API_KEY = 'your_api_key_here';
+const BASE_URL = 'https://app.apacta.com/api/v1';
 
-        // Placeholder for calculation logic
-        console.log(`Calculating materials for a ${projectType} with dimensions ${dimensions} in ${units} units.`);
-        
-        // Further implementation will be added here
+// Function to perform a GET request
+async function getData(endpoint) {
+    const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        handleApiError(error);
     }
+}
 
-    // Event listener for the form submission
-    document.getElementById('material-calculator-form').addEventListener('submit', calculateMaterials);
+// Function to perform a POST request
+async function postData(endpoint, data) {
+    const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}`;
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        handleApiError(error);
+    }
+}
 
+// Function to handle API errors
+function handleApiError(error) {
+    if (error instanceof Error) {
+        console.error('API Error:', error.message);
+    } else if (error.data && error.data.code === 422) {
+        const validationErrors = error.data.errors;
+        Object.keys(validationErrors).forEach((field) => {
+            console.error(`Validation failed for ${field}: ${validationErrors[field].join(', ')}`);
+            // Implement functionality to display these errors to the user
+        });
+    } else {
+        console.error('An unknown error occurred:', error);
+    }
+}
+
+// Example usage
+// Ensure these calls are made in the appropriate event handlers or lifecycle methods
+document.addEventListener('DOMContentLoaded', () => {
+    // Initialize the app or perform an initial data fetch
 });
+
+// Example event listener for a form submission
+document.querySelector('#yourFormId').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const data = {
+        // Construct your data object based on the form inputs
+    };
+    const result = await postData('/yourEndpoint', data);
+    if (result) {
+        // Handle the successful result
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('submit-button').addEventListener('click', handleSubmit);
+    document.querySelectorAll('.input-field').forEach(input => {
+        input.addEventListener('change', handleInputChange);
+    });
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target.form);
+    // Process formData and make API request
+    makeApiRequest(formData);
+}
+
+function handleInputChange(event) {
+    // Logic for handling input change
+}
+
+function makeApiRequest(data) {
+    // Perform the API request and update DOM accordingly
+}
+
+function updateDom(data) {
+    // Update the DOM with data from the API
+}
