bb9fb6e12f25c21c2d74798d6ff05ce493b9794d
Kennedy-oss <144157641+Kennedy-oss@users.noreply.github.com>
2023-11-06 17:44:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,94 @@
-// Replace 'your_api_key_here' with your actual API key from Apacta settings
-const API_KEY = 'your_api_key_here';
-const BASE_URL = 'https://app.apacta.com/api/v1';
+// main.js
 
-// Function to perform a GET request
-async function getData(endpoint) {
-    const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}`;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        handleApiError(error);
+document.addEventListener('DOMContentLoaded', function() {
+    // Define the base URL for your local JSON Server API
+    const apiUrl = 'http://localhost:3000';  // Default JSON Server URL
+
+    // Function to handle form submissions
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        // Logic to collect form data and send to the server
+        const formData = {
+            // Populate with actual form data
+            hours: document.getElementById('hoursInput').value,
+            materials: document.getElementById('materialsInput').value,
+            // Add more fields as necessary
+        };
+
+        // Call function to create a new record
+        createRecord(formData);
     }
-}
 
-// Function to perform a POST request
-async function postData(endpoint, data) {
-    const url = `${BASE_URL}${endpoint}?api_key=${API_KEY}`;
-    try {
-        const response = await fetch(url, {
+    // Function to create a new record in db.json
+    function createRecord(data) {
+        fetch(`${apiUrl}/records`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Update the DOM with new record or give user feedback
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        handleApiError(error);
     }
-}
 
-// Function to handle API errors
-function handleApiError(error) {
-    if (error instanceof Error) {
-        console.error('API Error:', error.message);
-    } else if (error.data && error.data.code === 422) {
-        const validationErrors = error.data.errors;
-        Object.keys(validationErrors).forEach((field) => {
-            console.error(`Validation failed for ${field}: ${validationErrors[field].join(', ')}`);
-            // Implement functionality to display these errors to the user
+    // Function to retrieve records from db.json
+    function getRecords() {
+        fetch(`${apiUrl}/records`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // Logic to display records on the DOM
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    // Function to update a record in db.json
+    function updateRecord(id, data) {
+        fetch(`${apiUrl}/records/${id}`, {
+            method: 'PUT',  // or 'PATCH'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // Logic to update the DOM
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
-    } else {
-        console.error('An unknown error occurred:', error);
     }
-}
 
-// Example usage
-// Ensure these calls are made in the appropriate event handlers or lifecycle methods
-document.addEventListener('DOMContentLoaded', () => {
-    // Initialize the app or perform an initial data fetch
-});
-
-// Example event listener for a form submission
-document.querySelector('#yourFormId').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const data = {
-        // Construct your data object based on the form inputs
-    };
-    const result = await postData('/yourEndpoint', data);
-    if (result) {
-        // Handle the successful result
+    // Function to delete a record from db.json
+    function deleteRecord(id) {
+        fetch(`${apiUrl}/records/${id}`, {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(() => {
+            console.log('Record deleted');
+            // Logic to handle the UI after deletion
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
     }
+
+    // Event listeners for your form
+    // Replace 'formSelector' with your actual form selector
+    document.querySelector('formSelector').addEventListener('submit', handleFormSubmit);
+
+    // Call getRecords on load to populate the page with data
+    getRecords();
 });
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('submit-button').addEventListener('click', handleSubmit);
-    document.querySelectorAll('.input-field').forEach(input => {
-        input.addEventListener('change', handleInputChange);
-    });
-});
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let formData = new FormData(event.target.form);
-    // Process formData and make API request
-    makeApiRequest(formData);
-}
-
-function handleInputChange(event) {
-    // Logic for handling input change
-}
-
-function makeApiRequest(data) {
-    // Perform the API request and update DOM accordingly
-}
-
-function updateDom(data) {
-    // Update the DOM with data from the API
-}
