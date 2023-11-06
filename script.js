0f6d1b08a9d916bc53b542e53438c7bde19e4c03
Kennedy-oss <144157641+Kennedy-oss@users.noreply.github.com>
2023-11-06 18:32:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-// main.js
-
 document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'http://localhost:3000';  // Default JSON Server URL
+    const apiUrl = 'http://localhost:3000'; // Default JSON Server URL
+
+    // Function to list all records
+    function getRecords() {
+        fetch(`${apiUrl}/records`)
+        .then(response => response.json())
+        .then(records => {
+            const tableBody = document.getElementById('recordsTable').getElementsByTagName('tbody')[0];
+            tableBody.innerHTML = ''; // Clear existing records
+            records.forEach(record => {
+                const row = tableBody.insertRow();
+                row.insertCell(0).textContent = record.hoursWorked;
+                row.insertCell(1).textContent = record.materialUsed;
+                row.insertCell(2).textContent = record.quality;
+
+                // Create Edit/Delete options
+                const actionsCell = row.insertCell(3);
+                const editButton = document.createElement('button');
+                editButton.textContent = 'Edit';
+                editButton.onclick = function() { editRecord(record.id); };
+                actionsCell.appendChild(editButton);
+
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Delete';
+                deleteButton.onclick = function() { deleteRecord(record.id); };
+                actionsCell.appendChild(deleteButton);
+            });
+        })
+        .catch(error => {
+            displayMessage(`Error: ${error}`, 'error');
+        });
+    }
 
     // Function to handle form submissions
     function handleFormSubmit(event) {
@@ -9,13 +38,13 @@ document.addEventListener('DOMContentLoaded', function() {
         const formData = {
             hoursWorked: document.getElementById('hoursWorked').value,
             materialUsed: document.getElementById('materialUsed').value,
-            quality: document.getElementById('quality').value, // Make sure this ID corresponds to a select or input field in your HTML for quality
+            quality: document.getElementById('quality').value,
         };
 
         createRecord(formData);
     }
 
-    // Function to create a new record in db.json
+    // Function to create a new record
     function createRecord(data) {
         fetch(`${apiUrl}/records`, {
             method: 'POST',
@@ -25,11 +54,32 @@ document.addEventListener('DOMContentLoaded', function() {
             body: JSON.stringify(data),
         })
         .then(response => response.json())
-        .then(data => {
+        .then(record => {
             displayMessage('Success: Record added', 'success');
-            // Optionally: Insert logic to add the new record to a list on the page
+            getRecords(); // Refresh the list with the new record
         })
         .catch((error) => {
+            displayMessage(`Error: ${error}`, 'error');
+        });
+    }
+
+    // Function to edit a record
+    function editRecord(id) {
+        // Logic to edit a record
+        // This can involve displaying the record data in the form for editing,
+        // or a separate modal/dialog for the user to update the record information
+    }
+
+    // Function to delete a record
+    function deleteRecord(id) {
+        fetch(`${apiUrl}/records/${id}`, {
+            method: 'DELETE',
+        })
+        .then(() => {
+            displayMessage('Record deleted successfully', 'success');
+            getRecords(); // Refresh the list to reflect the deletion
+        })
+        .catch(error => {
             displayMessage(`Error: ${error}`, 'error');
         });
     }
@@ -44,6 +94,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Event listener for the form submission
     document.getElementById('workRegisterForm').addEventListener('submit', handleFormSubmit);
 
-    // Call getRecords on load to populate the page with data
+    // Initial call to populate the table with records
     getRecords();
 });
