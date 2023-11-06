583e46bf15bba1caa8de7d744130fe95e4e10633
Kennedy-oss <144157641+Kennedy-oss@users.noreply.github.com>
2023-11-06 18:36:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apiUrl = 'http://localhost:3000'; // Default JSON Server URL
+    let isEditMode = false;
+    let currentEditId = null;
 
     // Function to list all records
     function getRecords() {
         fetch(`${apiUrl}/records`)
-        .then(response => response.json())
-        .then(records => {
-            const tableBody = document.getElementById('recordsTable').getElementsByTagName('tbody')[0];
-            tableBody.innerHTML = ''; // Clear existing records
-            records.forEach(record => {
-                const row = tableBody.insertRow();
-                row.insertCell(0).textContent = record.hoursWorked;
-                row.insertCell(1).textContent = record.materialUsed;
-                row.insertCell(2).textContent = record.quality;
+            .then(response => response.json())
+            .then(records => renderRecords(records))
+            .catch(error => displayMessage(`Error: ${error}`, 'error'));
+    }
 
-                // Create Edit/Delete options
-                const actionsCell = row.insertCell(3);
-                const editButton = document.createElement('button');
-                editButton.textContent = 'Edit';
-                editButton.onclick = function() { editRecord(record.id); };
-                actionsCell.appendChild(editButton);
+    function renderRecords(records) {
+        const tableBody = document.getElementById('recordsTable').getElementsByTagName('tbody')[0];
+        tableBody.innerHTML = ''; // Clear existing records
+        records.forEach(record => {
+            const row = tableBody.insertRow();
+            row.insertCell(0).textContent = record.hoursWorked;
+            row.insertCell(1).textContent = record.materialUsed;
+            row.insertCell(2).textContent = record.quality;
 
-                const deleteButton = document.createElement('button');
-                deleteButton.textContent = 'Delete';
-                deleteButton.onclick = function() { deleteRecord(record.id); };
-                actionsCell.appendChild(deleteButton);
-            });
-        })
-        .catch(error => {
-            displayMessage(`Error: ${error}`, 'error');
+            // Create Edit/Delete options
+            const actionsCell = row.insertCell(3);
+            const editButton = document.createElement('button');
+            editButton.textContent = 'Edit';
+            editButton.onclick = function() { editRecord(record); };
+            actionsCell.appendChild(editButton);
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.onclick = function() { deleteRecord(record.id); };
+            actionsCell.appendChild(deleteButton);
         });
+    }
+
+    // Function to populate form with record's data for editing
+    function populateFormForEdit(data) {
+        document.getElementById('hoursWorked').value = data.hoursWorked;
+        document.getElementById('materialUsed').value = data.materialUsed;
+        document.getElementById('quality').value = data.quality;
+        currentEditId = data.id;
+        isEditMode = true;
     }
 
     // Function to handle form submissions
@@ -41,33 +52,51 @@ document.addEventListener('DOMContentLoaded', function() {
             quality: document.getElementById('quality').value,
         };
 
-        createRecord(formData);
+        if (isEditMode) {
+            updateRecord(currentEditId, formData);
+        } else {
+            createRecord(formData);
+        }
     }
 
-    // Function to create a new record
-    function createRecord(data) {
-        fetch(`${apiUrl}/records`, {
-            method: 'POST',
+    // Function to create or update a new record
+    function createOrUpdateRecord(data, id = null) {
+        const method = id ? 'PUT' : 'POST';
+        const url = id ? `${apiUrl}/records/${id}` : `${apiUrl}/records`;
+
+        fetch(url, {
+            method: method,
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data),
         })
-        .then(response => response.json())
-        .then(record => {
-            displayMessage('Success: Record added', 'success');
-            getRecords(); // Refresh the list with the new record
-        })
-        .catch((error) => {
-            displayMessage(`Error: ${error}`, 'error');
-        });
+            .then(response => response.json())
+            .then(() => {
+                displayMessage('Success: Record has been processed', 'success');
+                resetForm();
+                getRecords(); // Refresh the list with the new record
+            })
+            .catch((error) => {
+                displayMessage(`Error: ${error}`, 'error');
+            });
+    }
+
+    // Function to reset form and mode
+    function resetForm() {
+        document.getElementById('workRegisterForm').reset();
+        isEditMode = false;
+        currentEditId = null;
     }
 
     // Function to edit a record
-    function editRecord(id) {
-        // Logic to edit a record
-        // This can involve displaying the record data in the form for editing,
-        // or a separate modal/dialog for the user to update the record information
+    function editRecord(record) {
+        populateFormForEdit(record);
+    }
+
+    // Function to update a record
+    function updateRecord(id, data) {
+        createOrUpdateRecord(data, id);
     }
 
     // Function to delete a record
@@ -75,13 +104,13 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch(`${apiUrl}/records/${id}`, {
             method: 'DELETE',
         })
-        .then(() => {
-            displayMessage('Record deleted successfully', 'success');
-            getRecords(); // Refresh the list to reflect the deletion
-        })
-        .catch(error => {
-            displayMessage(`Error: ${error}`, 'error');
-        });
+            .then(() => {
+                displayMessage('Record deleted successfully', 'success');
+                getRecords(); // Refresh the list to reflect the deletion
+            })
+            .catch(error => {
+                displayMessage(`Error: ${error}`, 'error');
+            });
     }
 
     // Function to display messages to the user
